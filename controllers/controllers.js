efbe7a014483d4ec3fbe62789465ce561c8e48d9
Brent Moseley <bmoseley@revspringinc.com>
2014-10-09 09:54:32 -0700

    Added border to visually show scope.

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -39,7 +39,7 @@ var module = angular.module('module', [])
     // Just a very simple directive to use for scope example
     return {
         restrict: 'E',     // restrict to only match on element names
-        scope: {        // isolate scope
+        scope: {           // isolate scope
           lettuce: "=info" // this binds different scope based on the info attribute
         },
         template: "<br>Directive: {{lettuce.name}} has {{lettuce.calories}} calories."   // Normally, this would be in an
@@ -53,7 +53,7 @@ var module = angular.module('module', [])
   return {
     restrict: 'A',     // restrict to only attribute directives
     template: "I see this: {{val}} and have this object: {{bread.content}}",
-    scope: {
+    scope: {          // isolate scope
     },    
     link: function(scope, element, attrs) {
       console.log('link ran')
@@ -63,41 +63,5 @@ var module = angular.module('module', [])
       scope.bread = new TableRow('bread absorbs ' + scope.val)
 
     }
-    
   }
-
-  //       scope.$watch(attrs.anotherDir, function(value) {
-  //           console.log ('watch triggered with: ' + value)
-  //       })
-
-  //     scope.addBlankRow = function(deleteIcon) {
-  //       deleteIcon = deleteIcon ? deleteIcon : false;
-  //       var header_new = angular.copy(scope.accountHeaders); // Angularjs makes a DEEP copy.
-  //       var editable = angular.forEach(header_new, function (item) {
-  //         item.type.field = item.type.field == 'span' ? 'input' : item.type.field
-  //         item.editable = true
-  //         item['blankRow'] = true
-  //       })
-  //       scope.paymentData.lineItems.push(new TableRow({}, editable, deleteIcon))
-  //     }
-
-  //     scope.deleteRow = function (index){
-  //       delete scope.accountsTableAmountValues[index]
-  //       delete scope.markedForPay[index];
-  //       scope.paymentData.lineItems.splice(index, 1);
-  //     }
-  //   }
-  // }
-
-  // // Create an object for each row by stitching together required, highlighted, or disabled requirments with
-  // // each repsectable item in row, plus create the nested object for form submission.
-  // function TableRow (row, tableHeaders, deleteIcon) {
-  //   this.DocumentId = row.document_id || null;
-  //   this.amount = '';
-  //   this.consumerAddedRow = _.isEmpty(row) && deleteIcon == true ? true : false;
-  //   this.lineItemDetails = row;
-  //   this.tableHeaders = convertDataField(tableHeaders);
-  // }
-
-
 })
